fa2e85403f6847bf8aeb15908a4d065d40898f31
RaresGIT <rares.petrisor1@gmail.com>
2021-12-03 10:44:31 +0100

    renamed to nft_module

--- NFT Module/frontend_app/src/context/index.js
+++
@@ -1,6 +0,0 @@
-/* global BigInt */
-
-import React from "react";
-
-export const nodeInfoContextDefaultValue = { networkIdentifier: "", genesisConfig: { minFeePerByte: BigInt(0) }, height: 0 };
-export const NodeInfoContext = React.createContext({ ...nodeInfoContextDefaultValue });
